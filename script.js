6ab1563d07a0185b5929f98fef014d4804452a68
oristotel <dorest28@gmail.com>
2019-09-10 09:18:58 +0300

    Task 3

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 'use strict';
 
-let money = +prompt('What is your budget for a month?', '3200'),
+let money, time;
+
+function start() {
+    money = +prompt('What is your budget for a month?', '3200');
     time = prompt('Input date in format: YYYY-MM-DD', '2019-01-01');
+
+    while (isNaN(money) || money == "" || money == null) {
+        money = +prompt('What is your budget for a month?', '3200');
+    }
+}
+start();
 
 let appData = {
     budget: money,
@@ -9,23 +18,26 @@ let appData = {
     expences: {},
     optionalExpences: {},
     income: {},
-    savings: false,
+    savings: true,
 };
 
-// for (let i = 0; i < 2; i++) {
-//     let a1 = prompt('Input an obligatory expense:', 'Mivina');
-//     let a2 = prompt('Input cost:', '55');
+function chooseExpences() {
+    for (let i = 0; i < 2; i++) {
+        let a1 = prompt('Input an obligatory expense:', 'Mivina');
+        let a2 = prompt('Input cost:', '55');
 
-//     if ((typeof(a1) === 'string') && (typeof(a1) != null) &&
-//         (typeof(a2) != null) && a1 != '' && a2 != '' &&
-//         a1.length < 50) {
-//         appData.expences[a1] = a2; // a1 - ключ, а2 - значення
-//     } else {
+        if ((typeof(a1) === 'string') && (typeof(a1) != null) &&
+            (typeof(a2) != null) && a1 != '' && a2 != '' &&
+            a1.length < 50) {
+            appData.expences[a1] = a2; // a1 - ключ, а2 - значення
+        } else {
 
-//     }
-// }
+        }
+    }
+}
+chooseExpences();
 
-let i = 0;
+//let i = 0;
 
 // while (i < 2) {
 //     let a1 = prompt('Input an obligatory expense:', 'Mivina');
@@ -41,32 +53,67 @@ let i = 0;
 //     i++;
 // }
 
-i = 0;
-do {
-    let a1 = prompt('Input an obligatory expense:', 'Mivina');
-    let a2 = prompt('Input cost:', '55');
+// i = 0;
+// do {
+//     let a1 = prompt('Input an obligatory expense:', 'Mivina');
+//     let a2 = prompt('Input cost:', '55');
 
-    if ((typeof(a1) === 'string') && (typeof(a1) != null) &&
-        (typeof(a2) != null) && a1 != '' && a2 != '' &&
-        a1.length < 50) {
-        appData.expences[a1] = a2; // a1 - ключ, а2 - значення
+//     if ((typeof(a1) === 'string') && (typeof(a1) != null) &&
+//         (typeof(a2) != null) && a1 != '' && a2 != '' &&
+//         a1.length < 50) {
+//         appData.expences[a1] = a2; // a1 - ключ, а2 - значення
+//     } else {
+
+//     }
+//     i++;
+// } while (i < 2);
+
+
+function chooseOptExpences() {
+    for (let i = 0; i < 3; i++) {
+        let opt = prompt('Input an optional expense:', 'MDMA');
+
+        if ((typeof(opt) === 'string') && (typeof(opt) != null) &&
+            opt.length < 50) {
+            appData.optionalExpences[i + 1] = opt; // a1 - ключ, а2 - значення
+        } else {
+
+        }
+    }
+}
+
+
+function detectDayBudget() {
+    appData.moneyPerDay = parseFloat((appData.budget / 30).toFixed(2));
+    alert('1 day budget is ' + appData.moneyPerDay);
+}
+detectDayBudget();
+
+function detectLevel() {
+    if (appData.moneyPerDay < 100) {
+        console.log("Minimal level of profit");
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+        console.log("Normal level of profit");
+    } else if (appData.moneyPerDay > 2000) {
+        console.log("High level of profit");
     } else {
+        console.log("WTF?");
+    }
+}
+detectLevel();
+
+function checkSavings() {
+    if (appData.savings == true) {
+        let save = +prompt('What is your value of savings?', '100'),
+            percent = +prompt('What is the percent value', '5');
+
+        appData.monthIncome = save / 100 / 12 * percent;
+        alert('The profit per month from your deposit: ' + appData.monthIncome);
 
     }
-    i++;
-} while (i < 2);
-
-appData.moneyPerDay = appData.budget / 30;
-
-alert('1 day budget is ' + appData.moneyPerDay);
-if (appData.moneyPerDay < 100) {
-    console.log("Minimal level of profit");
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-    console.log("Normal level of profit");
-} else if (appData.moneyPerDay > 2000) {
-    console.log("High level of profit");
-} else {
-    console.log("WTF?");
 }
+checkSavings();
+
+
 
 console.log(appData);
